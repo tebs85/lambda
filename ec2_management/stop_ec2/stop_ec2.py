80a1c37cc3e7a143cb509ebe90fa1add557070ec
Tebogo Moloi <tebogo.molo2@standardbank.co.za>
2021-09-10 20:35:00 +0200

    feat: added method get all instanes by tag

--- ec2_management/stop_ec2/stop_ec2.py
+++ ec2_management/stop_ec2/stop_ec2.py
@@ -23,11 +23,32 @@ def get_all_instanes_id():
         
     return all_instances
 
+# Get all instances tags (auto start/stop)
+def get_all_instanes_by_tag():
+    
+    all_instances = []
+    
+    describe_instances = ec2.describe_instances()
+    
+    for instances in describe_instances['Reservations']:
+        for instance in instances['Instances']:
+            for tag in instance['Tags']:
+                # print(tag['Key'] == "autoManageInstanceSatus")
+                if tag['Key'] == "autoManageInstanceSatus":
+                    if tag['Value'] == 'start':
+                        all_instances.append(instance['InstanceId'])
+        
+    return all_instances
+    
 def lambda_handler(event, context):
+    
     if instances == 0:
-        ec2.stop_instances(InstanceIds=instances)
-        print('stopped your instances: ' + str(instances))
+        ec2.start_instances(InstanceIds=get_all_instanes_by_tag())
+        print('started your instances: ' + str(get_all_instanes_by_tag()))
+    elif get_all_instanes_by_tag() == 0: 
+        ec2.start_instances(InstanceIds=get_all_instanes_id())
+        print('started your instances: ' + str(get_all_instanes_id()))
     else:
-        ec2.stop_instances(InstanceIds=get_all_instanes_id())
-        print('stopped your instances: ' + str(get_all_instanes_id()))
+        ec2.start_instances(InstanceIds=instances)
+        print('started your instances: ' + str(instances))
 
